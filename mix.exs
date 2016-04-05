23407c228c8ac26ef5c7e2c540214ee397d99ee6
Eduardo Gurgel <eduardo@carnivallabs.com>
2016-04-06 11:58:43 +1200

    Bump version 1.3.0
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GCM.Mixfile do
 
   def project do
     [app: :gcm,
-     version: "1.2.0",
+     version: "1.3.0",
      elixir: "~> 1.0",
      name: "GCM",
      description: @description,
